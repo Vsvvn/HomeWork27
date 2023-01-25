2564c7180510896dcd0a10f6dfa53709957bda1c
Vsvvn <Vladsun@yandex.ru>
2023-01-25 07:09:46 +0300

    Первый коммит.

---
+++ src/main/java/vs/recipe/HomeWork27Application.java
@@ -0,0 +1,13 @@
+package vs.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork27Application.class, args);
+    }
+
+}
---
+++ src/test/java/vs/recipe/HomeWork27ApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
